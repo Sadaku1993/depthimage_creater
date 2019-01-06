9a099630fbcee8380991852592c296504c3b9a0f
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2019-01-06 19:14:42 +0900

    update

--- scripts/bagrec/bag_rec_map.sh
+++ scripts/bagrec/bag_rec_map.sh
@@ -14,6 +14,7 @@ ODOM="/odom"
 IMU="/imu/data"
 LCL="/cloud/lcl"
 RM_HUMAN="/cluster/human/removed"
+NORMAL="/cloud_normal"
 
 echo $TIME &
 # echo $CLOUD &
@@ -21,5 +22,7 @@ echo $ODOM &
 echo $IMU &
 echo $LCL &
 echo $RM_HUMAN &
+echo $NORMAL &
 
-/opt/ros/kinetic/bin/rosbag record $ODOM $IMU $LCL $RM_HUMAN -O /home/amsl/$TIME.bag
+# /opt/ros/kinetic/bin/rosbag record $ODOM $IMU $RM_HUMAN -O /home/amsl/$TIME.bag
+/opt/ros/kinetic/bin/rosbag record $ODOM $IMU $NORMAL -O /home/amsl/$TIME.bag
